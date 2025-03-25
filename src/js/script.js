d9bd98df17087084066cdb6baa0c468747fd8f68
Aliaksandr Bykouski <developweb34@gmail.com>
2025-03-25 20:18:48 +0100

    add swiper 1/2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,42 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 import "purecss/build/grids-min.css";
 import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+
+try {
+    new Swiper('.swiper', {
+        slidesPerView: 1,
+        loop: true, //чтоб слайды повторялись
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 1200px
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            1920: {
+                spaceBetween: 35
+            }
+        },
+
+        modules: [Navigation, Pagination],
+
+    });
+} catch (error) {
+    console.error(error.message);
+}
+
+
+
